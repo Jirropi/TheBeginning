f15a771ac10b0629e84c6219be1643817e4e4c1b
Jirropi <jirropi@live.com>
2018-11-07 16:15:03 +0800

    Added username, password and enable prompt

--- NetworkFiles/Cisco-Autobackup.py
+++ NetworkFiles/Cisco-Autobackup.py
@@ -1,1 +1,11 @@
-import netmiko
+import os.path
+import getpass
+from netmiko import ConnectHandler
+
+username = input("Username: ")
+password = getpass.getpass("Password: ")
+enable = getpass.getpass("Enable: ")
+
+print(username)
+print(password)
+print(enable)
